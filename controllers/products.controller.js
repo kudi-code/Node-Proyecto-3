173db831809d34d99bc5ed689a9e3cf130a47889
Kevin Salvador Casas <kevinsal.casas@gmail.com>
2022-05-31 09:10:05 -0500

    adding purchases

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,64 +1,94 @@
 const { Product } = require('../models/product.model');
 const { AppError } = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
-const { } = require('../models/category.model')
+const { Category } = require('../models/category.model');
 
 const createProduct = catchAsync(async (req, res, next) => {
-  const {title, description,quantity, price, categoryId} = req.body
+  const { title, description, quantity, price, categoryId } = req.body;
 
-  const {sessionUser} = req
+  const { sessionUser } = req;
 
-  if(sessionUser===undefined){
-    return next(new AppError('You must init session', 400));
+  if (sessionUser === undefined) {
+    return next(new AppError('You must log in', 400));
   }
-  const product = await Product.create({title, description, quantity,price,categoryId,userId: sessionUser.id})
+  const product = await Product.create({
+    title,
+    description,
+    quantity,
+    price,
+    categoryId,
+    userId: sessionUser.id,
+  });
 
   res.status(200).json({
     status: 'done!',
-    product
-  })
-
+    product,
+  });
 });
 
 const getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.findAll({where: {status: 'active'}})
+  const products = await Product.findAll({ where: { status: 'active' } });
 
   res.status(200).json({
-    products
-  })
+    products,
+  });
 });
 
 const getProductById = catchAsync(async (req, res, next) => {
-  const {id} = req.params
+  const { id } = req.params;
 
-  const product = await Product.findOne({where: {id,status: 'active'}})
+  const product = await Product.findOne({ where: { id, status: 'active' } });
 
   res.status(200).json({
-    product
-  })
+    product,
+  });
 });
 const updateProduct = catchAsync(async (req, res, next) => {
-  const {title, description,quantity, price, categoryId} = req.body
+  const { title, description, quantity, price, categoryId } = req.body;
   //Product obtained from ProtectProductOwner
-  const {product} = req
+  const { product } = req;
 
-  product.update({title,description,quantity,price,categoryId})
+  product.update({ title, description, quantity, price, categoryId });
 
   res.status(200).json({
     status: 'done!',
-    product
-  })
-
+    product,
+  });
 });
 
 const deleteProduct = catchAsync(async (req, res, next) => {
-  const {product} = req
+  //Product obtained from ProtectProductOwner
+  const { product } = req;
 
-  product.update({status:'deleted'})
+  product.update({ status: 'deleted' });
 
   res.status(200).json({
-    status: 'deleted!'
-  })
+    status: 'deleted!',
+  });
+});
+
+const getAllCategories = catchAsync(async (req, res, next) => {
+  const categories = await Category.findAll({ where: { status: 'active' } });
+  res.status(200).json({
+    categories,
+  });
+});
+const createCategory = catchAsync(async (req, res, next) => {
+  const {name} = req.body
+  await Category.create({name})
+
+  const categories = await Category.findAll({ where: { status: 'active' } });
+
+  res.status(200).json({
+    status: 'created!',
+    categories,
+  });
+});
+const updateCategory = catchAsync(async (req, res, next) => {
+  const categories = await Category.findAll({ where: { status: 'active' } });
+  res.status(200).json({
+    categories,
+  });
 });
 
 module.exports = {
@@ -67,4 +97,7 @@ module.exports = {
   createProduct,
   updateProduct,
   deleteProduct,
+  getAllCategories,
+  createCategory,
+  updateCategory,
 };
